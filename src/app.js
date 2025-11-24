4925ace2abc7aca35cbe2613c04577ebaebf05f4
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-11-24 13:28:03 +0200

    a

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import {
+  defaultModules,
+  success,
+  error,
+} from "../node_modules/@pnotify/core/dist/PNotify.js";
+import * as PNotifyMobile from "../node_modules/@pnotify/mobile/dist/PNotifyMobile.js";
+import "@pnotify/core/dist/BrightTheme.css";
+defaultModules.set(PNotifyMobile, {});
+
+success({
+  title: 'Success!',
+  text: 'That thing that you were trying to do worked.'
+});
